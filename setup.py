2eab7de27e88e6daecaf6c3ebe79f4396d0d278e
kamilbilous <bruhleq@gmail.com>
2024-12-19 08:55:51 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+version = os.getenv(
+    "VERSION", "0.0.0"
+)  # Fallback to '0.0.0'version = os.getenv('PACKAGE_VERSION', '0.0.0')  # Fallback to '0.0.0'
+
+
+
+setup(
+    name="Attendance_list",
+    version=version,
+    author="Kamil Biłous",
+    author_email="bruhleq@gmail.com",
+    description="Attendance list managment";
+    url="https://github.com/fburczyk/Attendance-List/tree/kamil_testing",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12",
+)
