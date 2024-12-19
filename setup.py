7cdfa113c14614dc1c95bc02bc0e94fc3dda1e68
fburczyk <97292357+fburczyk@users.noreply.github.com>
2024-12-19 08:58:23 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+version = os.getenv(
+    "VERSION", "0.0.0"
+)  # Fallback to '0.0.0'version = os.getenv('PACKAGE_VERSION', '0.0.0')  # Fallback to '0.0.0'
+
+
+
+setup(
+    name="fburczyk",
+    version=version,
+    author="Filip Burczyk",
+    author_email="filip.b0311@gmail.com",
+    description="Attendance list",
+    url="https://github.com/fburczyk/Attendance-List/tree/filip_testing",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12.8",
+)
